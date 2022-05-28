491c1bf7bfeb03fab156c1464d57292409059a94
Omar Magdy <omar.professional8777@gmail.com>
2022-05-28 16:37:09 +0200

    Edited pscontests.sh

--- scripts/.scripts/pscontests.sh
+++ scripts/.scripts/pscontests.sh
@@ -8,5 +8,5 @@ cd ${REPLY}
 for p in $PROBLEMS
 do
   touch $p.cpp
-  echo -e "#include<bits/stdc++.h>\n\nusing namespace std\n\nint main () {\n\n}" > $p.cpp
+  echo -e "#include<bits/stdc++.h>\n\nusing namespace; std\n\nint main () {ios_base::sync_with_stdio(false);\ncin.tie(NULL);\nint tt;\nwhile(tt--){\n}}" > $p.cpp
 done
