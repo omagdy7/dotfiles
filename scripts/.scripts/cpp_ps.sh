3c949f51a4ab72db64265b4f88ee625f6935744e
Omar Magdy <omar.professional8777@gmail.com>
2022-05-28 16:30:20 +0200

    Added a new script that creates a cpp file with the headers I use for competitive programming and added an alias that runs that script

---
+++ scripts/.scripts/cpp_ps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+touch $1.cpp
+echo -e "#include<bits/stdc++.h>\n\nusing namespace std;\n\nint main () {\n\tios_base::sync_with_stdio(false);\n\tcin.tie(NULL);\n\tint tt;\n\twhile(tt--){\n\n\t}\n}" > $1.cpp
+nvim $1.cpp
+
+
