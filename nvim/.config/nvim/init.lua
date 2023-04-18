a7ec66a76975b8e8ad855f6e414813c8ae5710d5
omagdy7 <omar.professional8777@gmail.com>
2023-04-18 16:48:20 +0200

    Moved to AstroVim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,34 +1,18 @@
-vim.defer_fn(function()
-  pcall(require, "impatient")
-end, 0)
-
-require "core"
-require "core.options"
-
--- setup packer + plugins
-local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1e222a" })
-  print "Cloning packer .."
-  fn.system { "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path }
-
-  -- install plugins + compile their configs
-  vim.cmd "packadd packer.nvim"
-  require "plugins"
-  vim.cmd "PackerSync"
-
-  -- install binaries from mason.nvim & tsparsers
-  vim.api.nvim_create_autocmd("User", {
-    pattern = "PackerComplete",
-    callback = function()
-      vim.cmd "bw | silent! MasonInstallAll" -- close packer window
-      require("packer").loader "nvim-treesitter"
-    end,
-  })
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
 end
 
-pcall(require, "custom")
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify("Error setting up colorscheme: " .. astronvim.default_colorscheme, "error")
+  end
+end
 
-require("core.utils").load_mappings()
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
--- nvim/.config/nvim/lua/plugins/configs/mason.lua
+++ nvim/.config/nvim/lua/plugins/configs/mason.lua
@@ -1,49 +1,20 @@
-local present, mason = pcall(require, "mason")
+return function(_, opts)
+  require("mason").setup(opts)
 
-if not present then
-  return
+  -- TODO: AstroNvim v4: change these auto command names to not conflict with core Mason commands
+  local cmd = vim.api.nvim_create_user_command
+  cmd("MasonUpdate", function(options) require("astronvim.utils.mason").update(options.fargs) end, {
+    nargs = "*",
+    desc = "Update Mason Package",
+    complete = "custom,v:lua.mason_completion.available_package_completion",
+  })
+  cmd(
+    "MasonUpdateAll",
+    function() require("astronvim.utils.mason").update_all() end,
+    { desc = "Update Mason Packages" }
+  )
+
+  for _, plugin in ipairs { "mason-lspconfig", "mason-null-ls", "mason-nvim-dap" } do
+    pcall(require, plugin)
+  end
 end
-
-vim.api.nvim_create_augroup("_mason", { clear = true })
-vim.api.nvim_create_autocmd("Filetype", {
-  pattern = "mason",
-  callback = function()
-    require("base46").load_highlight "mason"
-  end,
-  group = "_mason",
-})
-
-local options = {
-  ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
-
-  PATH = "skip",
-
-  ui = {
-    icons = {
-      package_pending = " ",
-      package_installed = " ",
-      package_uninstalled = " ﮊ",
-    },
-
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
-      cancel_installation = "<C-c>",
-    },
-  },
-
-  max_concurrent_installers = 10,
-}
-
-options = require("core.utils").load_override(options, "williamboman/mason.nvim")
-
-vim.api.nvim_create_user_command("MasonInstallAll", function()
-  vim.cmd("MasonInstall " .. table.concat(options.ensure_installed, " "))
-end, {})
-
-mason.setup(options)
