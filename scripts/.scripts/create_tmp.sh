fa049ca745ae91e4e3c1835f40535efac4e6cc23
omagdy7 <omar.professional8777@gmail.com>
2024-05-01 02:37:25 +0300

    Now opens the temp file in alacritty instead of kitty

--- scripts/.scripts/create_tmp.sh
+++ scripts/.scripts/create_tmp.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 # Choose an extenstion for the temp file
-choice=$(echo -e "cpp\nc\npy\nrs\njs\nts\ngo\ntxt\nmd\nhtml\ncss"| dmenu -p "Select extension: " -l 10)
+choice=$(echo -e "cpp\nc\npy\nrs\njs\nts\ngo\ntxt\nmd\nhtml\ncss" | dmenu -p "Select extension: " -l 10)
 
 # choose random word from a dictionary
 random_word=$(cat /usr/share/dict/cracklib-small | shuf -n 1)
 
 # create temp file and open it in nvim
-kitty -e nvim /tmp/$random_word.$choice
-
-
+alacritty -e nvim /tmp/$random_word.$choice
