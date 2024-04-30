a18badc752e246cb95ded09017ac8cc6bde4acc3
omagdy7 <omar.professional8777@gmail.com>
2024-05-01 02:38:26 +0300

    Now Saves all screenshots to /tmp in addition to copying the image to the clipboard

--- scripts/.scripts/screenshot.sh
+++ scripts/.scripts/screenshot.sh
@@ -2,3 +2,5 @@
 
 selection=$(hacksaw -f "-i %i -g %g")
 shotgun $selection - | xclip -t 'image/png' -selection clipboard
+random_word=$(cat /usr/share/dict/cracklib-small | shuf -n 1)
+shotgun $selection - >/tmp/$random_word.png
